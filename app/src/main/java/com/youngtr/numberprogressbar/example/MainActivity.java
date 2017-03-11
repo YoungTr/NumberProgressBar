542027eebe552034369e8746ba97e6febb6c7899
YoungTr <tangyan1004@gmail.com>
2017-03-11 21:59:57 +0800

    add some styles

--- app/src/main/java/com/youngtr/numberprogressbar/example/MainActivity.java
+++ app/src/main/java/com/youngtr/numberprogressbar/example/MainActivity.java
@@ -10,15 +10,24 @@ import com.youngtr.numberprogressbar.NumberProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private NumberProgressBar mHorizontalProgressBar;
-    private NumberProgressBar mCircleProgressBar;
+    private NumberProgressBar mHorizontalProgressBarRed;
+    private NumberProgressBar mHorizontalProgressBarBlue;
+    private NumberProgressBar mHorizontalProgressBarYellow;
+    private NumberProgressBar mCircleProgressBarBlue;
+    private NumberProgressBar mCircleProgressBarYellow;
+    private NumberProgressBar mCircleProgressBarRed;
 
     private Handler mTimedHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-            if (mCircleProgressBar.getProgress() <= 100) {
-                mCircleProgressBar.setProgress(mCircleProgressBar.getProgress() + 1);
-                mHorizontalProgressBar.setProgress(mHorizontalProgressBar.getProgress() + 1);
+            if (mHorizontalProgressBarYellow.getProgress() <= 100) {
+                int progress = mHorizontalProgressBarYellow.getProgress() + 1;
+                mHorizontalProgressBarRed.setProgress(progress);
+                mHorizontalProgressBarBlue.setProgress(progress);
+                mHorizontalProgressBarYellow.setProgress(progress);
+                mCircleProgressBarBlue.setProgress(progress);
+                mCircleProgressBarYellow.setProgress(progress);
+                mCircleProgressBarRed.setProgress(progress);
                 mTimedHandler.sendEmptyMessageDelayed(0, 100);
             }
         }
@@ -29,8 +38,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mHorizontalProgressBar = (NumberProgressBar) findViewById(R.id.progress_horizontal);
-        mCircleProgressBar = (NumberProgressBar) findViewById(R.id.progress_circle);
+        mHorizontalProgressBarRed = (NumberProgressBar) findViewById(R.id.progress_horizontal_red);
+        mHorizontalProgressBarBlue = (NumberProgressBar) findViewById(R.id.progress_horizontal_blue);
+        mHorizontalProgressBarYellow = (NumberProgressBar) findViewById(R.id.progress_horizontal_yellow);
+        mCircleProgressBarBlue = (NumberProgressBar) findViewById(R.id.progress_circle_blue);
+        mCircleProgressBarYellow = (NumberProgressBar) findViewById(R.id.progress_circle_yellow);
+        mCircleProgressBarRed = (NumberProgressBar) findViewById(R.id.progress_circle_red);
 
         mTimedHandler.sendEmptyMessage(0);
     }
